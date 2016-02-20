df99c622612133c9ca0c3242ae41302f356cbe7a
tomaspinos <tomas.pinos@gmail.com>
2016-02-20 21:39:38 +0100

    Unit tests fixed.

--- src/test/java/cz/repaymentplan/logic/calendar/StaticWorkdayCheckerTest.groovy
+++ src/test/java/cz/repaymentplan/logic/calendar/StaticWorkdayCheckerTest.groovy
@@ -1,7 +1,6 @@
 package cz.repaymentplan.logic.calendar
 
 import org.junit.Test
-import static cz.repaymentplan.logic.calendar.StaticWorkdayChecker.getDateParser
 import static cz.repaymentplan.logic.enums.Country.CZE
 import static cz.repaymentplan.logic.enums.Country.SVK
 import static org.junit.Assert.assertFalse
@@ -17,25 +16,25 @@ class StaticWorkdayCheckerTest {
     void isWorkday_CZE() {
         def checker = new StaticWorkdayChecker()
 
-        assertFalse(checker.isWorkday(CZE, dateParser.parseDateTime("1.1.2000")))
+        assertFalse(checker.isWorkday(CZE, StaticWorkdayChecker.dateParser.parseDateTime("1.1.2000")))
 
-        assertFalse(checker.isWorkday(CZE, dateParser.parseDateTime("24.12.2012")))
-        assertFalse(checker.isWorkday(CZE, dateParser.parseDateTime("25.12.2012")))
-        assertFalse(checker.isWorkday(CZE, dateParser.parseDateTime("26.12.2012")))
+        assertFalse(checker.isWorkday(CZE, StaticWorkdayChecker.dateParser.parseDateTime("24.12.2012")))
+        assertFalse(checker.isWorkday(CZE, StaticWorkdayChecker.dateParser.parseDateTime("25.12.2012")))
+        assertFalse(checker.isWorkday(CZE, StaticWorkdayChecker.dateParser.parseDateTime("26.12.2012")))
 
-        assertTrue(checker.isWorkday(CZE, dateParser.parseDateTime("23.5.2012")))
+        assertTrue(checker.isWorkday(CZE, StaticWorkdayChecker.dateParser.parseDateTime("23.5.2012")))
     }
 
     @Test
     void isWorkday_SVK() {
         def checker = new StaticWorkdayChecker()
 
-        assertFalse(checker.isWorkday(SVK, dateParser.parseDateTime("26.12.2000")))
+        assertFalse(checker.isWorkday(SVK, StaticWorkdayChecker.dateParser.parseDateTime("26.12.2000")))
 
-        assertFalse(checker.isWorkday(CZE, dateParser.parseDateTime("24.12.2012")))
-        assertFalse(checker.isWorkday(CZE, dateParser.parseDateTime("25.12.2012")))
-        assertFalse(checker.isWorkday(CZE, dateParser.parseDateTime("26.12.2012")))
+        assertFalse(checker.isWorkday(CZE, StaticWorkdayChecker.dateParser.parseDateTime("24.12.2012")))
+        assertFalse(checker.isWorkday(CZE, StaticWorkdayChecker.dateParser.parseDateTime("25.12.2012")))
+        assertFalse(checker.isWorkday(CZE, StaticWorkdayChecker.dateParser.parseDateTime("26.12.2012")))
 
-        assertTrue(checker.isWorkday(SVK, dateParser.parseDateTime("23.5.2012")))
+        assertTrue(checker.isWorkday(SVK, StaticWorkdayChecker.dateParser.parseDateTime("23.5.2012")))
     }
 }
